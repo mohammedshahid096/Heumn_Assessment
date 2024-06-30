621f974d15c5740a3c5e490291580705d4b2e897
Mohammed shahid <84904302+mohammedshahid096@users.noreply.github.com>
2024-06-30 17:06:34 +0530

    Delete GraphQl directory

--- GraphQl/mutation.js
+++
@@ -1,7 +0,0 @@
-const { AddNewBookService } = require("../Services/book.service");
-const { AddNewUserService } = require("../Services/user.service");
-
-module.exports = {
-  addNewUser: AddNewUserService,
-  addNewBook: AddNewBookService,
-};
